2c280bf74622291bf4034430cc0147f2917dc74c
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-02 10:55:14 -0500

    A small working function

---
+++ hey_you.py
@@ -0,0 +1,11 @@
+def hey_you():
+    response = input('Well Hello, what is your name?')
+    print('Hello {}'.format(response))
+
+
+def main():
+    hey_you()
+
+
+if __name__ == '__main__':
+    main()
