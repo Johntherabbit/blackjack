0f1252b9724a009864b0fd9b150f4e391247072e
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-02 13:10:45 -0500

    better working version grade averages

--- grader.py
+++ grader.py
@@ -6,14 +6,33 @@ def input_grades():
         if response == 'quit':
             break
         elif response == 'grade':
-            print(round(float(sum(grades) / len(grades)), 2))
-            grades.clear()
+            average = round(float(sum(grades) / len(grades)), 2)
+            return average
         else:
             grades.append(int(response))
 
 
+def final_grade(average):
+    if average >= 90.0:
+        print("Your average is {} and your final grade is {}".format(
+            average, 'A'))
+    elif average >= 80.0:
+        print("Your average is {} and your final grade is {}".format(
+            average, 'B'))
+    elif average >= 70.0:
+        print("Your average is {} and your final grade is {}".format(
+            average, 'C'))
+    elif average >= 65.0:
+        print("Your average is {} and your final grade is {}".format(
+            average, 'D'))
+    elif average < 65.0:
+        print("Your average is {} and your final grade is {}".format(
+            average, 'F'))
+
+
 def main():
-    input_grades()
+    average = input_grades()
+    final_grade(average)
 
 
 if __name__ == '__main__':
