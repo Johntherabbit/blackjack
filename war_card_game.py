27dfd86dba5a66eddacf0a2e5f2694085d0b8fe3
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-03 13:38:19 -0500

    updated version of my original that adds piles along with the deck

--- war_card_game.py
+++ war_card_game.py
@@ -9,49 +9,67 @@ def deal():
     ]
     shuffle(cards)
     deck_1 = cards[26:]
-    new_deck_1 = []
+    pile_1 = []
     deck_2 = cards[:26]
-    new_deck_2 = []
+    pile_2 = []
+    #cards = []
 
     while True:
-
         x = deck_1.pop()
         y = deck_2.pop()
 
-        print("Player 1: Drew {}\nvs\nPlayer 2: Drew {}".format(x, y))
+        print(
+            "Player 1: Drew {}\nLength of Hand: {}\nvs\nPlayer 2: Drew {}\nLength of Hand: {}".
+            format(x, len(deck_1), y, len(deck_2)))
         print()
-        #input()
+        input()
+        #cards.extend([x, y])
 
-        if x > y:
-            new_deck_1.extend([x, y])
+        if len(deck_1) == 0:
+            shuffle(pile_1)
+            deck_1 = pile_1.copy()
+            pile_1 = []
+            print('Player two wins!')
+            return
+
+        elif len(deck_2) == 0:
+            shuffle(pile_2)
+            deck_2 = pile_2.copy()
+            pile_2 = []
+            print('Player one wins!')
+            return
+
+        elif x > y:
+            pile_1.extend([x, y])
         elif x < y:
-            new_deck_2.extend([x, y])
+            pile_2.extend([x, y])
 
-        if deck_1 == []:
-            shuffle(new_deck_1)
-            x = new_deck_1.pop()
-            y = new_deck_2.pop()
-            if x > y:
-                deck_1.extend([x, y])
-            elif new_deck_1 == []:
-                print('$$$$$$$$')
-                print('Player 2 Wins!')
-                print('$$$$$$$$')
-                print()
-            return
+    #while True:
 
-        if deck_2 == []:
-            shuffle(new_deck_2)
-            x = new_deck_1.pop()
-            y = new_deck_2.pop()
-            if x < y:
-                deck_2.extend([x, y])
-            elif new_deck_2 == []:
-                print('$$$$$$$$')
-                print('Player 1 Wins!')
-                print('$$$$$$$$')
-                print()
-            return
+
+#
+#    x = deck_1.pop()
+#    y = deck_2.pop()
+#
+#    print("Player 1: Drew {}\nvs\nPlayer 2: Drew {}".format(x, y))
+#    print()
+#    input()
+#    if deck_1 == []:
+#        print('$$$$$$$$')
+#        print('Player 2 Wins!')
+#        print('$$$$$$$$')
+#        print()
+#        return
+#    elif deck_2 == []:
+#        print('$$$$$$$$')
+#        print('Player 1 Wins!')
+#        print('$$$$$$$$')
+#        print()
+#        return
+#    elif x > y:
+#        deck_1.extend([x, y])
+#    elif x < y:
+#        deck_2.extend([x, y])
 
 
 def main():
