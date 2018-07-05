663cd7641ebd72a06c2ab6793840a7929ab43d28
basecamp <jmorgan@basecampcodingacademy.org>
2018-07-05 14:17:55 -0500

    Working version of blackjack card game

--- blackjack.py
+++ blackjack.py
@@ -1,31 +1,108 @@
 from random import shuffle
+from time import sleep
 
 
-def deal(list, deck):
-    list.append(deck.pop())
+def card_total(hand):
+    non_aces = []
+    aces = 0
+    for card in hand:
+        if card == 'Ace':
+            aces += 1
+        else:
+            non_aces.append(card)
+    total = sum(non_aces)
+    if aces == 0:
+        return total
+    elif aces == 1:
+        if total == 10:
+            return 21
+        elif total < 10:
+            return total + 11
+        else:
+            return total + 1
+    elif aces == 2:
+        if total == 9:
+            return 21
+        elif total < 9:
+            return total + 12
+        else:
+            return total + 2
+        return 21
+    elif aces == 3:
+        if total == 8:
+            return 21
+        elif total < 8:
+            return total + 13
+        else:
+            return total + 3
+    else:
+        if total == 7:
+            return 21
+        elif total < 7:
+            return total + 14
+        else:
+            return total + 4
+
+
+def deal(hand, deck):
+    hand.append(deck.pop())
 
 
 def dealer_call(dealer, deck):
-    total = sum(dealer)
-    if total < 17:
-        return deal(dealer, deck)
-    elif total >= 17:
-        return total
+    print('dealer\'s play')
+    total = card_total(dealer)
+    print(dealer, ' --> ', total)
+    while total < 17:
+        sleep(.500)
+        deal(dealer, deck)
+        total = card_total(dealer)
+        print(dealer, ' --> ', total)
+
+    if total > 21:
+        print('The dealer has busted!')
 
 
 def players_input(you, deck):
     while True:
         response = input('Would you like another card? ')
-        if response == 'yes':
-            return deal(you, deck)
+        print()
+        if response.lower().strip() == 'yes':
+            deal(you, deck)
             print(you)
+            print(card_total(you))
+            if card_total(you) > 21:
+                print('You have busted!')
+                break
+        elif response.lower().strip() == 'no':
+            break
+        else:
+            print('invalid input')
 
 
-def main():
+def winner(you, dealer):
+    if card_total(dealer) == 21:
+        print('The dealer has won!')
+    elif card_total(you) == 21:
+        print('You are the winner!')
+    elif card_total(dealer) > 21:
+        print('You are the winner!')
+    elif card_total(you) > 21:
+        print('Dealer won...')
+    elif card_total(dealer) > card_total(you) and card_total(dealer) <= 21:
+        print('The dealer has won!')
+    elif card_total(you) > card_total(dealer) and card_total(you) <= 21:
+        print('You are the winner!')
+    elif card_total(you) == card_total(dealer):
+        print('Tied Game!')
+
+
+def blackjack():
+    # the_cards = {'King': 10, 'Queen': 10, 'Jack': 10}
+
     deck = [
-        11, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 11, 10, 10, 10, 9, 8, 7, 6, 5,
-        4, 3, 2, 11, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 11, 10, 10, 10, 9, 8,
-        7, 6, 5, 4, 3, 2
+        'Ace', 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 'Ace', 10, 10, 10, 9, 8, 7,
+        6, 5, 4, 3, 2, 'Ace', 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2, 'Ace', 10,
+        10, 10, 9, 8, 7, 6, 5, 4, 3, 2
     ]
 
     shuffle(deck)
@@ -38,11 +115,23 @@ def main():
         deal(you, deck)
         deal(you, deck)
         break
-    print("Your Hand {}".format(you))
+    print("Your Hand {}\nTotal: {}".format(you, card_total(you)))
+    print()
     print("Dealer's Hand [{},?]".format(dealer[1]))
+    print()
 
-    dealer_call(dealer, deck)
     players_input(you, deck)
+    # if card_total(you) <= 21:
+    dealer_call(dealer, deck)
+    winner(you, dealer)
+    print()
+    print("Your Total: {}\nThe Dealers Total: {}".format(
+        card_total(you), card_total(dealer)))
+
+
+def main():
+    # while True:
+    blackjack()
 
 
 if __name__ == '__main__':
